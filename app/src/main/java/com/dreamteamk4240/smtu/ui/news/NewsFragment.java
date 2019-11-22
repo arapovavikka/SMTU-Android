84d46b647f6c81faa108d1994fef2333282d8c97
kristinatr <kristinatroilova@gmail.com>
2019-11-23 00:41:33 +0300

    News section

--- app/src/main/java/com/dreamteamk4240/smtu/ui/news/NewsFragment.java
+++ app/src/main/java/com/dreamteamk4240/smtu/ui/news/NewsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,6 +16,8 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.dreamteamk4240.smtu.R;
 
+
+
 public class NewsFragment extends Fragment {
 
     private NewsViewModel newsViewModel;
@@ -22,14 +26,28 @@ public class NewsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         newsViewModel =
                 ViewModelProviders.of(this).get(NewsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_news, container, false);
-        final TextView textView = root.findViewById(R.id.text_news);
-        newsViewModel.getText().observe(this, new Observer<String>() {
+
+        View root = inflater.inflate(R.layout.news_view, container, false);
+       // final TextView textView = root.findViewById(R.id.text_news);
+       /* newsViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        }); */
+        LinearLayout newsList = root.findViewById(R.id.news_list);
+        for (int i = 0; i <= 4; i++)
+        {
+            View view = inflater.inflate(R.layout.fragment_news, newsList, false);
+            TextView textViewTitle = view.findViewById(R.id.title_news);
+            textViewTitle.setText(newsViewModel.getTitleNews());
+            TextView textViewDate = view.findViewById(R.id.date_news);
+            textViewDate.setText(newsViewModel.getDateNews());
+            ImageView imageView = view.findViewById(R.id.main_image_news);
+            imageView.setImageResource(R.mipmap.image_news);
+            newsList.addView(view);
+        }
         return root;
     }
+
 }
--- app/src/main/java/com/dreamteamk4240/smtu/ui/news/NewsViewModel.java
+++ app/src/main/java/com/dreamteamk4240/smtu/ui/news/NewsViewModel.java
@@ -1,5 +1,9 @@
 package com.dreamteamk4240.smtu.ui.news;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,13 +11,27 @@ import androidx.lifecycle.ViewModel;
 public class NewsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private String titleNews;
+    private Calendar dateNews;
+
 
     public NewsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("Раздел Новости");
+        titleNews = "Заголовок новости";
+        dateNews = new GregorianCalendar();
+        dateNews = Calendar.getInstance();
+
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+    public String getTitleNews() {
+        return titleNews;
+    }
+    public String getDateNews() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMM yyyy");
+        return dateFormat.format(dateNews.getTime());
     }
 }
