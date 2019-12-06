beca823c1f67134c3959439e1d1dd159edfb243f
kristinatr <kristinatroilova@gmail.com>
2019-12-07 00:48:08 +0300

    Layout Contacts and logic with test data.

--- app/src/main/java/com/dreamteamk4240/smtu/ui/contacts/СontactsFragment.java
+++ app/src/main/java/com/dreamteamk4240/smtu/ui/contacts/СontactsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,6 +16,8 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.dreamteamk4240.smtu.R;
 
+import java.util.ArrayList;
+
 public class СontactsFragment extends Fragment {
 
     private СontactsViewModel сontactsViewModel;
@@ -22,14 +26,62 @@ public class СontactsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         сontactsViewModel =
                 ViewModelProviders.of(this).get(СontactsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_contacts, container, false);
-        final TextView textView = root.findViewById(R.id.text_contacts);
+        View root = inflater.inflate(R.layout.contacts, container, false);
+       /* final TextView textView = root.findViewById(R.id.text_contacts);
         сontactsViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        }); */
+        ArrayList<СontactsViewModel> contactsList = сontactsViewModel.getContacts();
+        for(СontactsViewModel item : contactsList) {
+            if (item.getName() == "Подготовительные курсы") {
+                CreateModelView(root, R.id.training_courses_list, item, inflater);
+            } else if (item.getName() == "Приемная комиссия") {
+                CreateModelView(root, R.id.selection_committee_list, item, inflater);
+            } else {
+                CreateModelView(root, R.id.department_of_payment_list, item, inflater);
+            }
+        }
         return root;
+    }
+    public void CreateFragments(LayoutInflater inflater, LinearLayout layout, String[] array, int imageId) {
+        int visible = 0;
+        if (array.length > 1) {visible = 1;}
+        for(String i : array) {
+            View view = inflater.inflate(R.layout.fragment_contacts, layout, false);
+            TextView textViewNumber = view.findViewById(R.id.text_contact);
+            textViewNumber.setText(i);
+            ImageView imageView = view.findViewById(R.id.image_contact);
+            imageView.setImageResource(imageId);
+            View delimeterView = view.findViewById(R.id.delimeter_view);
+            delimeterView.setVisibility(visible);
+            layout.addView(view);
+        }
+    }
+    public void CreateModelView (View root, int layoutId, СontactsViewModel item, LayoutInflater inflater){
+        LinearLayout contactList = root.findViewById(layoutId);
+        String[] phones = item.getPhones();
+        String[] emails = item.getEmails();
+        String[] adresses = item.getAdresses();
+        String schedule = item.getSchedule();
+        if (phones != null) {
+            CreateFragments(inflater, contactList,phones,R.mipmap.phone);
+        }
+        if (emails != null) {
+            CreateFragments(inflater, contactList,emails,R.mipmap.email);
+        }
+        if (adresses != null) {
+            CreateFragments(inflater, contactList,adresses,R.mipmap.adress);
+        }
+        if (schedule != null) {
+            View view = inflater.inflate(R.layout.fragment_contacts, contactList, false);
+            TextView textView = view.findViewById(R.id.text_contact);
+            textView.setText(schedule);
+            ImageView imageView = view.findViewById(R.id.image_contact);
+            imageView.setImageResource(R.mipmap.schedule);
+            contactList.addView(view);
+        }
     }
 }
--- app/src/main/java/com/dreamteamk4240/smtu/ui/contacts/СontactsViewModel.java
+++ app/src/main/java/com/dreamteamk4240/smtu/ui/contacts/СontactsViewModel.java
@@ -1,5 +1,8 @@
 package com.dreamteamk4240.smtu.ui.contacts;
 
+import java.util.ArrayList;
+import java.lang.String;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,6 +10,11 @@ import androidx.lifecycle.ViewModel;
 public class СontactsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private String name;
+    private String[] phones;
+    private String[] emails;
+    private String[] adresses;
+    private String schedule;
 
     public СontactsViewModel() {
         mText = new MutableLiveData<>();
@@ -15,5 +23,39 @@ public class СontactsViewModel extends ViewModel {
 
     public LiveData<String> getText() {
         return mText;
+    }
+    public String[] getPhones() {
+        return phones;
+    }
+    public String[] getEmails() {
+        return emails;
+    }
+    public String[] getAdresses() {
+        return adresses;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSchedule() {
+        return schedule;
+    }
+    public ArrayList<СontactsViewModel> getContacts() {
+        ArrayList<СontactsViewModel> contacts = new ArrayList<СontactsViewModel>();
+        СontactsViewModel contact1 = new СontactsViewModel();
+        contact1.name = "Подготовительные курсы";
+        contact1.phones = new String[]{"89377204556","89995296487","89991236544"};
+        contacts.add(contact1);
+        contact1 = new СontactsViewModel();
+        contact1.name = "Приемная комиссия";
+        contact1.phones = new String[]{"89375641232"};
+        contact1.adresses = new String[]{"Санкт-Петербург, Ленинский пр., 101"};
+        contact1.emails = new String[]{"priem@gmail.com", "priem@yandex.ru"};
+        contacts.add(contact1);
+        contact1 = new СontactsViewModel();
+        contact1.name = "Отдел оплаты образовательных услуг";
+        contact1.phones = new String[]{"89275412365","88005253535"};
+        contact1.adresses = new String[]{"с 10:00 до 17:00 по рабочим дням"};
+        contacts.add(contact1);
+        return contacts;
     }
 }
