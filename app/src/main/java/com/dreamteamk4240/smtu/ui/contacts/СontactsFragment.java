69f0d43ed98fb0afdf9f9f6a8e7b5d9761aaff1b
Vika <arapova.vikka@yandex.ru>
2019-12-07 05:05:22 +0300

    Clickable phone in contacts

--- app/src/main/java/com/dreamteamk4240/smtu/ui/contacts/СontactsFragment.java
+++ app/src/main/java/com/dreamteamk4240/smtu/ui/contacts/СontactsFragment.java
@@ -1,5 +1,7 @@
 package com.dreamteamk4240.smtu.ui.contacts;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -46,6 +49,7 @@ public class СontactsFragment extends Fragment {
         }
         return root;
     }
+
     public void CreateFragments(LayoutInflater inflater, LinearLayout layout, String[] array, int imageId) {
         int visible = 0;
         if (array.length > 1) {visible = 1;}
@@ -60,6 +64,51 @@ public class СontactsFragment extends Fragment {
             layout.addView(view);
         }
     }
+
+    public void CreateFragments(LayoutInflater inflater, LinearLayout layout, String[] array, int imageId, boolean isPhoneNumber) {
+        int visible = 0;
+        if (array.length > 1) {visible = 1;}
+        for(String i : array) {
+            View view = inflater.inflate(R.layout.fragment_contacts, layout, false);
+            TextView textViewNumber = view.findViewById(R.id.text_contact);
+            textViewNumber.setText(i);
+            if (isPhoneNumber)
+                textViewNumber.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        OpenPhone(((TextView)v).getText().toString());
+                    }
+                });
+            ImageView imageView = view.findViewById(R.id.image_contact);
+            imageView.setImageResource(imageId);
+            View delimeterView = view.findViewById(R.id.delimeter_view);
+            delimeterView.setVisibility(visible);
+            layout.addView(view);
+        }
+    }
+
+    public void OpenPhone(String phone)
+    {
+        // Create the intent and set the data for the
+        // intent as the phone number.
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.setData(Uri.parse("tel:" + phone));
+
+        try
+        {
+            // Launch the Phone app's dialer with a phone
+            // number to dial a call.
+            startActivity(i);
+        }
+        catch (SecurityException s)
+        {
+            // show() method display the toast with
+            // exception message.
+            Toast.makeText(getActivity(),"Error", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
     public void CreateModelView (View root, int layoutId, СontactsViewModel item, LayoutInflater inflater){
         LinearLayout contactList = root.findViewById(layoutId);
         String[] phones = item.getPhones();
@@ -67,13 +116,13 @@ public class СontactsFragment extends Fragment {
         String[] adresses = item.getAdresses();
         String schedule = item.getSchedule();
         if (phones != null) {
-            CreateFragments(inflater, contactList,phones,R.mipmap.phone);
+            CreateFragments(inflater, contactList,phones,R.mipmap.phone, true);
         }
         if (emails != null) {
-            CreateFragments(inflater, contactList,emails,R.mipmap.email);
+            CreateFragments(inflater, contactList,emails,R.mipmap.email, false);
         }
         if (adresses != null) {
-            CreateFragments(inflater, contactList,adresses,R.mipmap.adress);
+            CreateFragments(inflater, contactList,adresses,R.mipmap.adress, false);
         }
         if (schedule != null) {
             View view = inflater.inflate(R.layout.fragment_contacts, contactList, false);
